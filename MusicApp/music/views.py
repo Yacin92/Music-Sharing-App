70e21680a4811a01b81290a6a9892bb4b72ead14
yacin92 <yacincheffai@yahoo.com>
2017-03-10 09:47:56 +0100

    adding album details

--- MusicApp/music/views.py
+++ MusicApp/music/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import Http404
 from .models import Album
 # Create your views here.
 
@@ -7,8 +7,13 @@ from .models import Album
 def index(request):
 
     all_albums = Album.objects.all()
-    context = {'all_albums':all_albums,}
+    context = {'all_albums':all_albums}
     return render(request, "music/index.html", context)
 
 def detail(request, album_id):
-    return HttpResponse("<h2>details for album number " + album_id + "</h2>")
+    try:
+        album = Album.objects.get(pk=album_id)
+        context = {'album':album}
+    except Album.DoesNotExist:
+        raise Http404('album does not exist')
+    return render (request, 'music/detail.html', context)
